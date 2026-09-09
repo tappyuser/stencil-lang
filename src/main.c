d1e88b298210930c33dd17824f4580f35fea9fbf
tappyuser <tappycodes@gmail.com>
2026-09-09 16:39:00 +0100

    Added the stencil_parser library

--- src/main.c
+++ src/main.c
@@ -8,23 +8,30 @@
 
 static string_t *read_file(string_t *sourcefile) {
   /// Takes a filename and reads the contents of the file
-  /// Returns a pointer to the filecontents
+  /// Returns a string_t pointer to the filecontents
 #define BUFLEN 1024
   char _buffer[BUFLEN];
-  FILE *fileptr = fopen(sourcefile->content, "r");
+  FILE *fileptr;
+  if ((fileptr = fopen(sourcefile->content, "r")) == nullptr) {
+    fclose(fileptr);
+    return nullptr;
+  }
   fread(_buffer, sizeof(char), BUFLEN, fileptr);
   fclose(fileptr);
 
   return string_create(_buffer, 0);
 }
 
-static string_view_t *get_token(const string_t *const text, size_t *_index) {
+/// TODO: Put the get_token and pasrse functions in the stencil_parser project
+/// and create a library for it
+static string_view_t *get_token(const string_t *const text,
+                                size_t *const _index) {
   bool isQuote = false; /// For parsing inside of a string
   char *contents = text->content;
-  string_view_t *token;
+  string_view_t *token; /// Contains the token
 
   /// index passes through the text and checks if  each character meets a
-  /// specific condition eg if `abc = 5;` abc, = and 5 are all tokens While
+  /// specific condition eg if `abc = 5;` abc, = and 5 are all tokens. While
   /// parsing _start_index would point to 'a', index would keep incrementing
   /// until it sees a space then it would stop to get the token 'abc'.
   size_t *index = _index;
@@ -37,23 +44,26 @@ static string_view_t *get_token(const string_t *const text, size_t *_index) {
   /// _start_index points to the start of a token
   size_t _start_index = *_index;
 
-  /// Get an identifier
+  /// Get an identifier or numeral literal
   if (isalnum(contents[*index])) {
     while (isalnum(contents[*index + 1]))
       (*index)++;
 
     token = string_view(contents, _start_index, *index + 1);
     prints(token->begin, token->size);
-  } else {
+  }
+
+  else {
     /// Handles semicolon
     if (contents[*index] == ';') {
       token = string_view(contents, _start_index, *index + 1);
       prints(token->begin, token->size);
     }
 
-    /// For reapeating non alphanumeric tokens like '**' '++' '--' '...'
+    /// For reapeating non alphanumeric tokens like '**' '++' '--' '...' '//'
     else if (contents[*index] == '*' || contents[*index] == '-' ||
-             contents[*index] == '+' || contents[*index] == '.') {
+             contents[*index] == '+' || contents[*index] == '.' ||
+             contents[*index] == '/') {
       while (contents[*index + 1] == contents[*index])
         (*index)++;
       token = string_view(contents, _start_index, *index + 1);
@@ -105,13 +115,17 @@ static string_t parse(const string_t *const filecontents) {
 }
 
 int main(int argc, char **argv) {
-  // TODO: Add a help message
+  // TODO: Add a help message when run without arguments or with the "--help"
   if (argc == 1) {
-    printf("No text file to parse\n");
+    fprintf(stderr, "No text file to parse\n");
     return EXIT_FAILURE;
   }
   string_t *sourcefile = string_create(argv[1], 0);
   string_t *const filecontents = read_file(sourcefile);
+  if (filecontents == nullptr) {
+    fprintf(stderr, "Error processing the file");
+    return EXIT_FAILURE;
+  }
 
   printf("%s\n\n------------------\n", filecontents->content);
 
